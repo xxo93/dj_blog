808d099efac9325ac6961b0a05d84c36b660f565
xxo93 <61908699+xxo93@users.noreply.github.com>
2021-06-07 11:39:50 +0800

    Update 跨excel合并sheet.py

--- scripts/跨excel合并sheet.py
+++ scripts/跨excel合并sheet.py
@@ -5,18 +5,22 @@ def getAllFilesOfWalk(dir):
     """
     @desc: 使用listdir循环遍历
     @author: ZhongMinWang 2021/6/7
+    :return: file_list
     """
     if not os.path.isdir(dir):
         print(dir)
         return
     dirlist = os.walk(dir)
+    file_list = []
     for root, dirs, files in dirlist:
-        print(root, dirs, files)
+        # print(root, dirs, files)
         for file in files:
-            print(os.path.join(root, file))
+            # print(os.path.join(root, file))
+            file_list.append(os.path.join(root, file))
+    return file_list
 
 
-def merge_sheet():
+def merge_sheet(file1, file2):
     """
     @desc: COM 接口操作Excel进行sheet复制到另一个Excel中
     @author: ZhongMinWang 2021/6/7
@@ -24,8 +28,8 @@ def merge_sheet():
     xlApp = Dispatch('Excel.Application')
     # xlApp.Visible = True
 
-    fileBase = os.path.abspath('器件替代模板.xlsx')
-    wb1 = xlApp.Workbooks.Open(fileBase)
+    excelBase = os.path.abspath(file1)
+    wb1 = xlApp.Workbooks.Open(excelBase)
 
     """
     sh1 = wb1.Worksheets('综述')  # 获得模板表
@@ -33,9 +37,9 @@ def merge_sheet():
     sh3 = wb1.Worksheets('通用信号规范')  # 获得模板表
     sh4 = wb1.Worksheets('版本说明')  # 获得模板表
     """
-    file = os.path.abspath('DRMOS器件替代模板.xlsx')
+    excel = os.path.abspath(file2)
 
-    wb2 = xlApp.Workbooks.Open(file)
+    wb2 = xlApp.Workbooks.Open(excel)
     for i in range(4, 0, -1):
         copy_sht = wb1.Worksheets(i)
 
@@ -48,3 +52,20 @@ def merge_sheet():
 
     wb1.Save()  # 保存excel
     wb1.Close()  # 关闭excel
+
+
+if __name__ == '__main__':
+    basepath = 'D:\项目资料\系统开发\替代测试系统开发\无线器件替代可视化协同平台（第三版）\硬件器件替代测试计划和报告模板'
+    file_list1 = getAllFilesOfWalk(basepath + '\电源类自动生成模板')
+    file_list2 = getAllFilesOfWalk(basepath + '\电源类自动生成模板')
+    file_list3 = getAllFilesOfWalk(basepath + '\电源类自动生成模板')
+    file_list = [*file_list1, *file_list2, *file_list3]
+    print(len(file_list))
+
+    basefile = basepath + '\器件替代模板.xlsx'
+    for i, file in enumerate(file_list, 1):
+        merge_sheet(basefile, file)
+        print(f'已合并{i}个项目')
+
+    print('-- over! --')
+
