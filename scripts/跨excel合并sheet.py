0f436505c0db42e23200a760613dc59e6873fae2
xxo93 <61908699+xxo93@users.noreply.github.com>
2021-06-09 15:07:53 +0800

    Update 跨excel合并sheet.py

--- scripts/跨excel合并sheet.py
+++ scripts/跨excel合并sheet.py
@@ -26,13 +26,15 @@ class ExcelCOM():
         self.xlApp = Dispatch('Excel.Application')
         # xlApp.Visible = True
 
-    def merge_sheet(self, file1, file2):
+    def merge_sheet(self, file1, file2, merge_sht_num=1, Before=True):
         """
         @desc: COM 接口操作Excel进行sheet复制到另一个Excel中
         @author: ZhongMinWang 2021/6/7
         """
         excelBase = os.path.abspath(file1)
         wb1 = self.xlApp.Workbooks.Open(excelBase)
+
+        # cp_type = {'Before': 'sheet'} if Before else {'After': 'sheet'}
 
         """
         sh1 = wb1.Worksheets('综述')  # 获得模板表
@@ -43,7 +45,8 @@ class ExcelCOM():
         excel = os.path.abspath(file2)
 
         wb2 = self.xlApp.Workbooks.Open(excel)
-        for i in range(4, 0, -1):
+        for i in range(merge_sht_num, 0, -1):
+            # 获取copy的sheet
             copy_sht = wb1.Worksheets(i)
             try:
                 # 获取目标sheet
@@ -52,9 +55,13 @@ class ExcelCOM():
                 # self.xlApp.Worksheets.Add().Name = 'sheet1'
                 pass
             else:
-                # 将copy_sht复制到目标sheet表之前的位置
-                copy_sht.Copy(Before=sheet)
-
+                # 默认将copy_sht复制到目标sheet表之前的位置
+                if Before:
+                    copy_sht.Copy(Before=sheet)
+                else:
+                    # todo 有疑问，向后添加会新打开一个excel, 而没有直接在尾部添加sheet， 与Before有差别
+                    copy_sht.Copy(After=sheet)
+                    wb2.Save()
                 wb2.Save()  # 保存excel
         wb2.Close()  # 关闭excel
 
@@ -64,22 +71,30 @@ class ExcelCOM():
 
 if __name__ == '__main__':
     basepath = 'D:\项目资料\系统开发\替代测试系统开发\无线器件替代可视化协同平台（第三版）\硬件器件替代测试计划和报告模板'
-    file_list1 = getAllFilesOfWalk(basepath + '\电源类自动生成模板')
+    # file_list1 = getAllFilesOfWalk(basepath + '\电源类自动生成模板')
+    file_list1 = getAllFilesOfWalk(basepath + '\电源类自动生成模板 - 副本')
     file_list2 = getAllFilesOfWalk(basepath + '\射频类自动生成模板')
     file_list3 = getAllFilesOfWalk(basepath + '\中频类自动生成模板')
     file_list = [
-        # *file_list1,
+        *file_list1,
         # *file_list2,
-        *file_list3,
+        # *file_list3,
     ]
     print(len(file_list))
 
-    basefile = basepath + '\器件替代模板.xlsx'
+    beforeFile = basepath + '\综述.xlsx'
+    afterFile = basepath + '\综述之后.xlsx'
 
     obj = ExcelCOM()
+
+
+    # for i, file in enumerate(file_list, 1):
+    #     obj.merge_sheet(beforeFile, file)
+    #     print(f'已向前合并第{i}个项目，{file}')
+
     for i, file in enumerate(file_list, 1):
-        print(file)
-        obj.merge_sheet(basefile, file)
-        print(f'已合并{i}个项目')
+        obj.merge_sheet(afterFile, file, Before=False)
+        # obj.merge_sheet(afterFile, file, merge_sht_num=3, Before=False)
+        print(f'已向后合并{i}个项目，{file}')
 
     print('-- over! --')
